3103d64f4438356e202833d6f40f5648555f9008
rafaelzorn <rafael.zorn@gmail.com>
2018-05-01 11:10:14 -0300

    Replaced ListView by FlatList

--- src/modules/heroes/components/Hero.js
+++ src/modules/heroes/components/Hero.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { TouchableOpacity, ImageBackground, Text } from 'react-native'
 import { formatHeroData } from '../utils/Heroes'
 import Styles from './styles/Hero'
 
-export class Hero extends Component {    
-    render() {
-        const navigation = this.props.navigation
-        const data = formatHeroData(this.props.data)
-
-        return (
-            <TouchableOpacity 
-                style={Styles.item}
-                activeOpacity={0.7}
-                onPress={() => navigation.navigate('HeroDetailScreen', { data })}
-            >
-                <ImageBackground style={Styles.itemImage} source={{ uri: data.thumbnail }}>
-                    <Text style={[Styles.itemText, Styles.itemTitle]}>{ data.name }</Text>
-                </ImageBackground>
-            </TouchableOpacity>
-        )
-    }
+const Hero = props => {
+    const navigation = props.navigation
+    const data = formatHeroData(props.data)
+	
+    return (
+		<TouchableOpacity 
+			style={Styles.item}
+			activeOpacity={0.7}
+			onPress={() => navigation.navigate('HeroDetailScreen', { data })}
+		>
+			<ImageBackground style={Styles.itemImage} source={{ uri: data.thumbnail }}>
+				<Text style={[Styles.itemText, Styles.itemTitle]}>{ data.name }</Text>
+			</ImageBackground>
+		</TouchableOpacity>
+    )
 }
+
+export default Hero
--- src/modules/heroes/containers/HeroesScreen.js
+++ src/modules/heroes/containers/HeroesScreen.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
-import { ListView } from 'react-native'
+import { FlatList } from 'react-native'
 import { connect } from 'react-redux'
 import { fetchHeroes } from '../../../redux/actions'
-import { Hero } from '../components/Hero'
+import Hero from '../components/Hero'
 import Styles from './styles/HeroesScreen'
 
 class HeroesScreen extends Component {
     constructor(props) {
         super(props)
-        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1.id !== r2.id })
         this.state = {
-            dataSource: ds.cloneWithRows(this.props.heroes)
+            heroes: []
         }
     }
 
@@ -20,21 +19,22 @@ class HeroesScreen extends Component {
 
     componentWillReceiveProps(nextProps) {
         if (nextProps.heroes.length > 0) {
-            const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1.id !== r2.id })
             this.setState({
-                dataSource: ds.cloneWithRows(nextProps.heroes)
+                heroes: nextProps.heroes
             })
         }
     }
     
     render() {
         return (
-			<ListView
+            <FlatList
                 style={Styles.container}
-				dataSource={this.state.dataSource}
-				renderRow={(data) => <Hero data={data} navigation={this.props.navigation} />}
-				enableEmptySections
-			/>
+                data={this.state.heroes}
+                renderItem={({ item }) => (
+                    <Hero data={item} navigation={this.props.navigation} />
+                )}
+                keyExtractor={item => item.id.toString()}
+            />
         )   
     }
 }
